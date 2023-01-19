10c9571b3082bbdaa00220094607aa1ca1a64942
Stephen Nicholas Swatman <stephen@v25.nl>
2023-01-19 19:46:15 +0100

    Enable fast compilation
    This should save us some time building containers. Since runtime
    performance doesn't really matter, this is worth it for now.

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ ENV STACK_RESOLVER=${SNAPSHOT_VERSION}
 ENV STACK_ROOT=/root/.stack
 
 RUN stack --resolver $STACK_RESOLVER update
-RUN stack --resolver $STACK_RESOLVER install hakyll
+RUN stack --resolver $STACK_RESOLVER install --fast hakyll
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ set -o errexit
 chown -R $(id -un):$(id -gn) ${GITHUB_WORKSPACE}
 chown -R $(id -un):$(id -gn) /github/home/
 
-stack --allow-different-user --resolver ${STACK_RESOLVER} build
+stack --allow-different-user --resolver ${STACK_RESOLVER} build --fast
 stack --allow-different-user --resolver ${STACK_RESOLVER} run -- build
